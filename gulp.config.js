e1dd6fe1ed3d68f7ec35b06e676dc46089cf8290
Deepak <deepakdhawan81@gmail.com>
2016-10-17 08:58:09 +0530

    correted express server

--- gulp.config.js
+++ gulp.config.js
@@ -1,6 +1,10 @@
 module.exports = function(){
+
+    var clientPath = './src/Edge/FrontEnd/';
     var config = {
-        alljs:['./src/**/*.js','./*.js']
+        alljs:['./src/**/*.js','./*.js'],
+        less: clientPath + 'styles/*.less',
+        styles:'./.tmp'
     };
     return config;
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var args = require('yargs').argv;
+var del = require('del');
 var $ = require('gulp-load-plugins')({lazy:true});
 var config = require('./gulp.config.js')();          
 
@@ -14,8 +15,31 @@ gulp.task('StartProject',function(){
     console.log('Project have been started');
 });
 
+gulp.task('styler', ['clean-styles'],function(){
+    log('Compiling and Creating CSSSS Files ');
+    return gulp.src(config.less) 
+            .pipe($.plumber())           
+            .pipe($.less())
+            .pipe($.autoprefixer())
+            .pipe(gulp.dest(config.styles));
+});
+
+gulp.task('clean-styles', function(){
+    log('cleaning styles');
+    var filepath = config.styles + '/*.css';
+    cleaningFolder(filepath);
+});
+
+function cleaningFolder(filepath){
+    log('clearing files from the path ->' + filepath);
+    //log(done);
+    del(filepath);
+}
+
+
 gulp.task('vet',function(){
-    return gulp.src(config.alljs)
+    log('cheking all the JS files for errors and hint for opimize code');
+    return gulp.src(config.alljs)   
                 .pipe($.if(args.verbose,$.print()))
                 .pipe($.jscs())
                 .pipe($.jshint())
@@ -23,14 +47,18 @@ gulp.task('vet',function(){
                 .pipe($.jshint.reporter('fail'));
 });
 
+gulp.task('less-watcher',function(){
+    log(config.less);
+    gulp.watch([config.less],['styler']);
+});
 
 function log(msg){
     if(typeof(msg) === 'object'){
         for(var item in msg){
             if(msg.hasOwnProperty(item)){
-                $.util().log($.util.colors.blue(msg[item]));                
+                $.util.log($.util.colors.blue(msg[item]));                
             }}
     } else{
-        $.util().log($.util.colors.blue(msg));
+        $.util.log($.util.colors.blue(msg));
     }
 }
